968520eaa9ec56908206f8673267ec620e885c6e
eleniltonprojetos <eleniltonprojetos@gmail.com>
2026-02-11 21:51:57 -0300

    Corrige o vinte.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-        name: 'Sistema de Frota',
-        short_name: 'Frota',
-        description: 'Sistema completo para gestão de frotas.',
-        theme_color: '#2563eb',
-        background_color: '#ffffff',
-        display: 'standalone',
-        scope: '/',
-        start_url: '/',
-        orientation: 'portrait',
-        icons: [
-          {
-            src: 'pwa-192x192.png', // Certifique-se que esta imagem existe em /public
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png', // Certifique-se que esta imagem existe em /public
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+    tailwindcss(),
   ],
 })
